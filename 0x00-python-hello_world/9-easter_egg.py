ab5d54163c1b2be0413ff5517bb1dee81a8166d4
Andrecast <acastrillonpuerta@gmail.com>
2021-04-26 18:20:50 -0500

    Write a Python script that prints “The Zen of Python”, by TimPeters, followed by a new line

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,12 +1,2 @@
 #!/usr/bin/python3
 import this
-'''
-So what exactly is the purpose of this?
-
-It also contains 4 variables:
-
-this.c which contains the number 97
-this.s which contains The Zen of Python encoded in rot13
-this.i which contains the number 25
-this.d which contains a dictionary
-'''
