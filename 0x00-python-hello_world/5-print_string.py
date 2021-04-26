8b2478ff24fc6018220934cf8209b91250baab18
Andrecast <acastrillonpuerta@gmail.com>
2021-04-26 17:08:38 -0500

    Complete this source code in order to print 3 times a string stored in the variable str, followed by its first 9 characters

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str*3)#another way: print("{}\n{}".format(str * 3, str[:9]))
-print("{}".format(str[:9]))
+print("{}\n{}".format(str * 3, str[:9]))
