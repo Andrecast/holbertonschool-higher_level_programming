3dab530c978ba6bb3d3425466328b2bf23a63904
Andrecast <acastrillonpuerta@gmail.com>
2021-04-26 23:36:11 -0500

    Technical interview preparation

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -15,6 +15,6 @@ typedef struct listint_s
 int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *list);
+void free_listint(listint_t *head);
 
 #endif
