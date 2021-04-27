3e8cd590fbb1872ae5f0ef48a819f659c2f0f6ac
Andrecast <acastrillonpuerta@gmail.com>
2021-04-27 15:57:44 -0500

    This program will assign a random signed number to the variable number each time it is executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+print(number)
+lastdig = number % 10
 if number >= 0:
-    if number % 10 > 5:
+    if lastdig > 5:
         print("Last digit of {} is {} and\
- is greater than 5".format(number, number % 10))
-    elif number % 10 == 0:
+ is greater than 5".format(number, lastdig))
+    elif lastdig == 0:
         print("Last digit of {} is {} and\
- is 0".format(number, number % 10))
-    elif (number % 10) < 6 and (number % 10) != 0:
+ is 0".format(number, lastdig))
+    elif (lastdig < 6) and (lastdig != 0):
         print("Last digit of {} is {} and\
- is less than 6 and not 0".format(number, number % 10))
+ is less than 6 and not 0".format(number, lastdig))
 else:
-    n = -number  # lo vuelvo positivo para que me calcule el módulo que quiero
-    # el modulo de un numero negativo es diferente
-    if n % 10 == 0:
+    lastdigneg = -number  # el modulo de un numero negativo es diferente
+    if lastdigneg % 10 == 0:
         print("Last digit of {} is {} and\
- is 0".format(number, -(n % 10)))
-    elif (n % 10) < 6 and (n % 10) != 0:
+ is 0".format(number, -(lastdigneg % 10)))
+    else:
         print("Last digit of {} is {} and\
- is less than 6 and not 0".format(number, -(n % 10)))
+ is less than 6 and not 0".format(number, -(lastdigneg % 10)))
+
+
