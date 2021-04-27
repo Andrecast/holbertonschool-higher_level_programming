b57f070c29acfedbe6c583b817db51b916411157
Andrecast <acastrillonpuerta@gmail.com>
2021-04-27 15:32:45 -0500

    This program will assign a random signed number to the variable number each time it is executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = number % 10
-if number < 0:
-    if lastdigit > 5:
-        print("Last digit of {} is -{} and \
- is greater than 5".format(number, lastdigit))
-    elif lastdigit == 0:
-        print("Last digit of {} is -{} and \
- is 0".format(number, lastdigit))
-    elif lastdigit < 6 and lastdigit != 0:
-        print("Last digit of {} is -{} and \
- is less than 6 and not 0".format(number, lastdigit))
+if number >= 0:
+    if number % 10 > 5:
+        print("Last digit of {} is {} and\
+ is greater than 5".format(number, number % 10))
+    elif number % 10 == 0:
+        print("Last digit of {} is {} and\
+ is 0".format(number, number % 10))
+    elif (number % 10) < 6 and (number % 10) != 0:
+        print("Last digit of {} is {} and\
+ is less than 6 and not 0".format(number, number % 10))
 else:
-    if lastdigit > 5:
-        print("Last digit of {} is {} and \
- is greater than 5".format(number, lastdigit))
-    elif lastdigit == 0:
-        print("Last digit of {} is {} and \
- is 0".format(number, lastdigit))
-    elif lastdigit < 6 and lastdigit != 0:
-        print("Last digit of {} is {} and \
- is less than 6 and not 0".format(number, lastdigit))
+    n = -number
+    if n % 10 == 0:
+        print("Last digit of {} is {} and\
+ is 0".format(number, -(n % 10)))
+    elif (n % 10) < 6 and (n % 10) != 0:
+        print("Last digit of {} is {} and\
+ is less than 6 and not 0".format(number, -(n % 10)))
