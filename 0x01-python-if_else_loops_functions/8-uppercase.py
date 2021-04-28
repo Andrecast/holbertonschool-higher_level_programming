b701070b1584d64de6b770b78f9c0a96d60800fe
Andrecast <acastrillonpuerta@gmail.com>
2021-04-27 23:27:32 -0500

    fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
         num = ord(str[i])
         if num > 96 and num < 123:
             num = num - 32  # aquí empiezan las mayúsculas
-            print("{:c}".format(num), end="")
-        else:
-            print(str[i], end="")
+        print("{:c}".format(num), end="")
     print()
