af534afaf1934f8bf4e40c15ce531a34b73b8b2f
Andrecast <acastrillonpuerta@gmail.com>
2021-04-27 22:36:56 -0500

    Taking into accout upper characters and numbers

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    for s in range(0, len(str)):
-        num = ord(str[s])
+    for i in range(0, len(str)):
+        num = ord(str[i])
         if num > 96 and num < 123:
             num = num - 32  # aquí empiezan las mayúsculas
-            print(chr(num), end="")
+            print("{:c}".format(num), end="")
+        else:
+            print(str[i], end="")
     print()
