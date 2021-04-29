6b4262c049d2d738c8055f46bec1ffedc718d29b
Andrecast <acastrillonpuerta@gmail.com>
2021-04-29 14:49:22 -0500

    using chr function

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    print("{:c}".format(alph), end="")
-# print("{}".format(chr(alph)), end="") utilizando la funcion chr
+    print("{}".format(chr(alph)), end="")
