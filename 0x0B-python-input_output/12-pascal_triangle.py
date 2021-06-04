3c477db233772428b713cb54d43bbdb64e9d9cd9
Andrecast <acastrillonpuerta@gmail.com>
2021-06-04 17:03:14 -0500

    Another way

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         colum = [1]
         for j in range(0, len(filas[i]) - 1):
-            colum.extend([ filas[i][j] + filas[i][j + 1] ])
+            colum.extend([filas[i][j] + filas[i][j + 1]])
         colum += [1]
         filas.append(colum)
     return filas
