045742931d79dfe9dcf65e9488ef4fd5dc5dd0da
Andrecast <acastrillonpuerta@gmail.com>
2021-06-05 15:35:57 -0500

    public method thet prints in stdout the Rectangle instance with the character #

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,13 +2,12 @@
 """
 rectangle
 """
- # se debe importar la clase, porq está en otro archivo
+#  se debe importar la clase, porq está en otro archivo
 from models.base import Base
 
+
 class Rectangle(Base):
-    """
-    class Rectangle that inherits from Base
-    """
+    """class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         # llamar el id de la super clase
         super().__init__(id)
@@ -24,7 +23,14 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """1. width setter, value será el nuevo valor"""
+        """
+        1. width setter, value será el nuevo valor
+        2. validation of all setter methods and instantiation
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -34,7 +40,14 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """1. height setter, value será el nuevo valor"""
+        """
+        1. width setter, value será el nuevo valor
+        2. validation of all setter methods and instantiation
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -44,7 +57,14 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """1. x setter, value será el nuevo valor"""
+        """
+        1. width setter, value será el nuevo valor
+        2. validation of all setter methods and instantiation
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -54,6 +74,23 @@ class Rectangle(Base):
 
     @x.setter
     def y(self, value):
-        """1. y setter, value será el nuevo valor"""
+        """
+        1. width setter, value será el nuevo valor
+        2. validation of all setter methods and instantiation
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
-    
+
+    def area(self):
+        """Function that returns the rectangle area"""
+        return (self.width * self.height)
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        rectangle = ""
+        for i in range(self.height):
+            rectangle = ('#' * self.width)
+            print (rectangle)
