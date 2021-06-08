2cfd3ae658a0a082b74522bdca84e8a1a439b82c
Andrecast <acastrillonpuerta@gmail.com>
2021-06-08 18:57:41 -0500

    base.py final

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """
 Class Base
 """
+import json
+import os.path
 
 
 class Base:
@@ -16,3 +18,51 @@ class Base:
         else:  # si no pasa un id, se asigna el del contador
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        dictionary to JSON string
+        es decir, pasa de ser dict a ser str, esto con json.dumps()
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return ("[]")
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Json string to file"""
+        list = []
+        if list_objs is not None:
+            list = [items.to_dictionary() for items in list_objs]
+        with open("{}.json".format(cls.__name__), "w") as file:
+            file.write(cls.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Json string to dictionary"""
+        if json_string is None or len(json_string) == 0:
+            return ("[]")
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Dictionary to instance"""
+        if cls.__name__ == "Rectangle":
+            holder = cls(1, 1)
+        if cls.__name__ == "Square":
+            holder = cls(1)
+        holder.update(**dictionary)
+        return holder
+
+    @classmethod
+    def load_from_file(cls):
+        """file to instances"""
+        if not os.path.exists(cls.__name__ + ".json"):
+            return []
+        with open(cls.__name__ + ".json", "r") as file:
+            stuff = cls.from_json_string(file.read())
+        return [cls.create(**index) for index in stuff]
+
+    
+        
