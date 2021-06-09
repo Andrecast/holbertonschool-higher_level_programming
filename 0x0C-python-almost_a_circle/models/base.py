9cd2dc94b63048a952de5176fd86e3a9f92ff3b2
Andrecast <acastrillonpuerta@gmail.com>
2021-06-09 14:08:59 -0500

    from_json_string, return without

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,7 +43,7 @@ class Base:
     def from_json_string(json_string):
         """Json string to dictionary"""
         if json_string is None or len(json_string) == 0:
-            return ("[]")
+            return ([])
         return json.loads(json_string)
 
     @classmethod
