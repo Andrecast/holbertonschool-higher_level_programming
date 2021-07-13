ce51f2e331f9a0b5fd7d7b77fc486af56225e5cd
Andrecast <acastrillonpuerta@gmail.com>
2021-07-13 02:45:18 +0000

    converts DB, first_table and field name in first table into UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,9 @@
+-- converts hbtn_0c_0 DB to UTF8
+-- convert DB, table first_table and field name into first_table
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
