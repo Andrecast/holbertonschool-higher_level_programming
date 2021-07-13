43606e3771d30cb535c38101774db6b103d0eb16
Andrecast <acastrillonpuerta@gmail.com>
2021-07-13 00:44:30 +0000

    with space after --

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
---Create database hbtn_0c_0
---If database already exists, the script shouldn´t fail
+-- Create database hbtn_0c_0
+-- If database already exists, the script shouldn´t fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
