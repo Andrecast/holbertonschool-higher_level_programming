c84533a541cbbd5ee4afb21bce92bd4ee8852519
Andrecast <acastrillonpuerta@gmail.com>
2021-07-14 16:36:07 +0000

    creates hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- 6. States table
 -- creates the DB hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
-name VARCHAR(256) NOT NULL;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
 
