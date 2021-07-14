0729d32252b8461a1e4c148db1412672731ebe4f
Andrecast <acastrillonpuerta@gmail.com>
2021-07-14 18:39:50 +0000

    subquery

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- 8. Cities of California
+-- list all the cities of California that can´t be found in the DB hbtn_0d_usa
+SELECT id, name
+FROM cities
+WHERE state_id IN (
+SELECT id
+FROM states
+WHERE name = 'California')
+ORDER BY id ASC;
+
