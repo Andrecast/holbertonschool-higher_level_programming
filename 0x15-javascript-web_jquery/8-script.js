acf796e8985e0340d1f7bde7f30e88b61815530d
Andrecast <acastrillonpuerta@gmail.com>
2021-09-23 07:48:02 +0700

    fetches and lists the title for all movies by using an URL

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (i, value) {
+    $('UL#list_movies').append('<li>' + value.title + '</li>');
+  });
+});
